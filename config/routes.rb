8f1fcae0f714670d734ab91e088467139b85a133
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-12-09 04:24:19 +0000

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  get 'lists/new'
+  post 'lists' => 'lists#create'
 end
